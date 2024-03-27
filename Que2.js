72cc96fee1c4b358b3cde5adc5521b5793b5f537
1kush-patel <kushpatel123.kp14@gmail.com>
2024-03-27 00:03:53 -0400

    recors limit and index chanegs

--- Que2.js
+++ Que2.js
@@ -35,7 +35,7 @@ app.get("/",function(req, res){
 app.get("/data", function (req, res) {
     Product.find()
         .then(function (products) {
-            res.render('allData', { data: JSON.parse(JSON.stringify(products)) }); // send the products data as JSON
+            res.render('allData', { data: JSON.parse(JSON.stringify(products)).slice(0,100) }); // send the products data as JSON
         })
         .catch(function (err) {
             res.status(500).send(err); // send 500 status code and the error message
